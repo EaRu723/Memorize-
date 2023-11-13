05b78e0bad2368b6d69bbd0f2da63895ef851b85
EaRu723 <y.andrearusso@gmail.com>
2023-11-13 13:15:33 -0500

    Cleaned up code per lecture 3

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,52 +10,21 @@ import SwiftUI
 }
 struct ContentView: View {
     let emojis:Array<String> = ["💀","👻","🎃","🧙‍♀️","😵","🦷","🥸","🫥","🫨","😈","👿","👹"]
-    @State var cardCount:  Int = 8
+   
     var body: some View {
-        VStack{
-            title
             ScrollView{
                 cards
             }
-            Spacer()
-            cardCountAdjusters
-        }
         .padding()
     }
-    var cardCountAdjusters: some View{
-        HStack{
-            cardRemover
-            Spacer()
-            cardAdder
-        }
-        .imageScale(.large)
-        .font(.largeTitle)
-    }
-    var title: some View{
-        Text("Memorize!").font(.largeTitle)
-    }
     var cards: some View{
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
-            ForEach(0..<cardCount, id: \.self){index in
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85))]) {
+            ForEach(emojis.indices, id: \.self){index in
                 CardView(content: emojis[index])
                     .aspectRatio(2/3, contentMode: .fit)
             }
         }
         .foregroundColor(.orange)
-    }
-    func cardCountAdjuster(by offset: Int, symbol: String) -> some View{
-        Button(action: {
-                cardCount += offset
-        }, label: {
-            Image(systemName: symbol)
-        })
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
-    }
-    var cardRemover: some View {
-        cardCountAdjuster(by: -1, symbol: "rectangle.fill.badge.minus")
-    }
-    var cardAdder: some View {
-        cardCountAdjuster(by:1, symbol: "rectangle.fill.badge.plus")
     }
 }
  
